9eae2abcf573feea9746e37c89718f6d9d1fbdc4
Dr.McHack <dr.mchack@gmail.com>
2020-04-24 17:17:37 +0200

    Add Distance and get player in radius method

--- src/modules/manager/PlayerManager.class.js
+++ src/modules/manager/PlayerManager.class.js
@@ -65,6 +65,41 @@ class PlayerManager extends EntityManager {
 
 
   /**
+   * getPlayerInRadius - get a list with all players in a specific radius from position
+   *
+   * @param {Position} position the middlepoint
+   * @param {number}   distance the maximal distance between middlepoint and player
+   *
+   * @return {array} A list with all players in radius
+   */
+  getPlayerInRadius(position, distance) {
+    let players = [];
+    this.getAllPlayers().forEach((player, i) => {
+      if(this.distanceBetweenPosition(player.position, position) < distance) {
+        players.push(player);
+      }
+    });
+    return players;
+  }
+
+
+  /**
+   * distanceBetweenPosition - Get the distance between two positions
+   *
+   * @param {Position} position1 The first position
+   * @param {Position} position2 The second position
+   *
+   * @return {number} Distance between positions
+   */
+  distanceBetweenPosition(position1, position2) {
+    let deltaX = position1.x - position2.x;
+    let deltaY = position1.y - position2.y;
+    let deltaZ = position1.z - position2.z;
+    return Math.sqrt(deltaX ^ 2 + deltaY ^ 2 + deltaZ ^ 2);
+  }
+
+
+  /**
    * createPlayer - Create a new player
    *
    * @param {MCClient} client A minecraft client
