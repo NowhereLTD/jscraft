cb0df46fa3df404d31cf419ec865d2b371e29fee
Dr.McHack <dr.mchack@gmail.com>
2020-04-26 17:59:54 +0200

    Fix Version to 1.8 and optimize Event Handling

--- src/modules/Entity/LivingEntity.class.js
+++ src/modules/Entity/LivingEntity.class.js
@@ -8,6 +8,8 @@ const Entity = require("./Entity.class.js");
 class LivingEntity extends Entity {
   constructor(id, uuid, type) {
     super(id, uuid, type);
+    this.packagePosition = new Position();
+    this.onGround = false;
   }
 }
 
