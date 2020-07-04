19d3b15847a9143b59e6830aece29ca9ce1c17f4
Dr.McHack <dr.mchack@gmail.com>
2020-07-04 12:07:01 +0200

    Add NBT Item Structure for parsing NBT data

---
+++ src/modules/Model/NBT/ItemStructure.class.js
@@ -0,0 +1,255 @@
+
+class ItemStructure {
+  constructor() {
+    // Init General
+    this.general = [];
+    this.general.damage = null;
+    this.general.unbreakable = null;
+    this.general.canDestroy = null;
+    this.general.customModelData = null;
+
+    // Block Tags
+    this.block = [];
+    this.block.canPlaceOn = null;
+    this.block.blockEntityTag = null;
+    this.block.blockStateTag = null;
+
+    // Enchantments
+    this.enchantments = [];
+    this.storedEnchantments = [];
+    this.repairCost = null;
+
+    // Attribute Modifiers
+    this.attributes = [];
+
+    // potion effect
+    this.potionEffect = [];
+    this.potionEffect.customPotionEffects = [];
+    this.potionEffect.potion = null;
+    this.potionEffect.customPotionColor = null;
+
+    // TODO: Crossbow
+
+    // Display
+    this.display = [];
+    this.display.display = [];
+    this.display.display.color = null;
+    this.display.display.name = null;
+    this.display.display.lore = [];
+    this.display.hideFlags = null;
+
+  }
+
+  /**
+   * createEnchantment - Create a new Item Enchantment
+   *
+   * @param  string id The enchantment id
+   * @param  int level The level of enchantment
+   */
+  createEnchantment(id, level) {
+    this.enchantments.push({id: { type: 'short', value: id}, lvl: { type: 'short', value: level}});
+  }
+
+
+  /**
+   * createStoredEnchantment - Create a enchantment for books
+   *
+   * @param  int id    the enchantment id
+   * @param  int level the level of enchantment
+   */
+  createStoredEnchantment(id, level) {
+    this.storedEnchantments.push({id: { type: 'short', value: id}, lvl: { type: 'short', value: level}});
+  }
+
+
+  /**
+   * createAttribute - Create a new attribute modifier to modifier item
+   *
+   * @param  string attributeName the name of attribute
+   * @param  string name          the name of modifier (has no effect)
+   * @param  string slot          the slot to take a effect ("mainhand", "offhand", "feet", "legs", "chest", "head")
+   * @param  int operation        modifier operation (add [id=0], multiply_base [id=1], multiply [id=2])
+   * @param  float amount         the amount of change
+   * @param  array uuid           int array with the uuid of modifier (four numbers exxample: [0, 0, 0, 0])
+   */
+  createAttribute(attributeName, name, slot, operation, amount, uuid) {
+    this.attributes.push({AttributeName: attributeName, Name: name, Slot: slot, Operation: operation, Amount: amount, UUID: uuid});
+  }
+
+
+  /**
+   * createPotionEffect - Create a new Potion effect
+   *
+   * @param  int id                id of effect
+   * @param  int amplifier         amplifier of the effect
+   * @param  int duration          duration of effect in ticks
+   * @param  boolean ambient       if this effect a bacon effect
+   * @param  boolean showParticles produces particles
+   * @param  boolean showIcon      show the effect icon
+   */
+  createPotionEffect(id, amplifier, duration, ambient, showParticles, showIcon) {
+    this.potionEffect.push({Id: id, Amplifier: amplifier, Duration: duration, Ambient: ambient, ShowParticles: showParticles, ShowIcon: showIcon});
+  }
+
+
+  /**
+   * getNBT - Get the NBT raw data from item structure
+   *
+   * @return JSON The json NBT data structure
+   */
+  getNBT() {
+    let data = {
+      type: "compound",
+      name: "",
+      value: {}
+    };
+
+    // Set general data
+    if(this.general.damage !== null) {
+      data.value.Damage = {
+        type: "int",
+        value: this.general.damage
+      }
+    }
+
+    if(this.general.unbreakable !== null) {
+      data.value.Unbreakable = {
+        type: "byte",
+        value: boolToNum(this.general.unbreakable)
+      }
+    }
+
+    if(this.general.canDestroy !== null) {
+      data.value.CanDestroy = {
+        type: "list",
+        value: {
+          type: "string",
+          value: this.general.canDestroy
+        }
+      }
+    }
+
+    if(this.general.customModelData !== null) {
+      data.value.CustomModelData = {
+        type: "int",
+        value: this.general.customModelData
+      }
+    }
+
+    if(this.block.canPlaceOn !== null) {
+      data.value.CanPlaceOn = {
+        type: "list",
+        value: {
+          type: "string",
+          value: this.general.canPlaceOn
+        }
+      }
+    }
+
+    // TODO: BlockEntityTag
+
+    // Enchantments
+    if(this.enchantments.length > 0) {
+      data.value.Enchantments = {
+        type: "list",
+        value: {
+          type: 'compound',
+          value: this.enchantments
+        }
+      }
+    }
+
+    if(this.storedEnchantments.length > 0) {
+      data.value.StoredEnchantments = {
+        type: "list",
+        value: {
+          type: 'compound',
+          value: this.storedEnchantments
+        }
+      }
+    }
+
+    if(this.repairCost !== null) {
+      data.value.repairCost = {
+        type: "int",
+        value: this.repairCost
+      }
+    }
+
+    // Set display data
+    let cacheData = this.getDisplayData();
+    if(cacheData !== null) {
+      data.value.display = cacheData;
+    }
+
+    // Set hide flags
+    if(this.display.hideFlags !== null) {
+      data.value.HideFlags = {
+        type: "int",
+        value: this.display.hideFlags
+      };
+    }
+
+    return data;
+  }
+
+  /**
+   * getDisplayData - Get the display NBT data
+   *
+   * @return JSON the NBT JSON data
+   */
+  getDisplayData() {
+    let displayData = {};
+    displayData.type = "compound";
+    displayData.value = {};
+
+    // Set color
+    if(this.display.display.color !== null) {
+      displayData.value.color = {
+        type: "int",
+        value: this.display.display.color
+      };
+    }
+
+    // Set name
+    if(this.display.display.name !== null) {
+      displayData.value.Name = {
+        type: "string",
+        value: this.display.display.name
+      };
+    }
+
+    // Set lore
+    if(this.display.display.lore.length > 0) {
+      displayData.value.Lore = {
+        type: "list",
+        value: {
+          type: "string",
+          value: this.display.display.lore
+        }
+      };
+    }
+
+    if(Object.keys(displayData.value) === 0) {
+      displayData = null;
+    }
+    return displayData;
+  }
+
+
+  /**
+   * boolToNum - Parse a boolean to a number
+   *
+   * @param  {type} bool description
+   * @return {type}      description
+   */
+  boolToNum(bool) {
+    if(bool) {
+      return 1;
+    }
+    return 0;
+  }
+}
+
+
+module.exports = ItemStructure;
